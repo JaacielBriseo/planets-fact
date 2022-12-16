e170b5ffb8868ea7d15c386abf714958bca36546
JaacielBriseo <jaacielbris23@gmail.com>
2022-12-15 21:47:12 -0700

    Hamburger button updated with proper svg icon

--- src/components/HamburgerButton.tsx
+++ src/components/HamburgerButton.tsx
@@ -1,14 +1,15 @@
 import { useAppSelector, useAppDispatch, toggleMobileMenu } from '../app';
-
 export const HamburgerButton = () => {
 	const { isMobileMenuOpen } = useAppSelector((state) => state.planets);
 	const dispatch = useAppDispatch();
 	return (
 		<div className='w-6 h-4'>
 			<button onClick={() => dispatch(toggleMobileMenu())} type='button' className='flex flex-col space-y-1'>
-				<span className={`${isMobileMenuOpen ? 'bg-DarkGray' : 'bg-White'} w-6 h-[3px]`}></span>
-				<span className={`${isMobileMenuOpen ? 'bg-DarkGray' : 'bg-White'} w-6 h-[3px]`}></span>
-				<span className={`${isMobileMenuOpen ? 'bg-DarkGray' : 'bg-White'} w-6 h-[3px]`}></span>
+				<svg xmlns='http://www.w3.org/2000/svg' width='24' height='17'>
+					<g fill={`${isMobileMenuOpen ? '#38384F' : '#FFF'}`} fillRule='evenodd'>
+						<path d='M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z' />
+					</g>
+				</svg>
 			</button>
 		</div>
 	);
